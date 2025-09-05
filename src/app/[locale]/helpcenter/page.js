059dbfd7d5dc5968b70072ebff0d8e3ebabe3d83
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-09-05 19:37:02 +0500

    Update page.js

--- src/app/[locale]/helpcenter/page.js
+++ src/app/[locale]/helpcenter/page.js
@@ -3,11 +3,11 @@ import React, { useState,useEffect } from "react";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
-const NavLight = dynamic(()=>import('../components/navlight'))
-const Footer = dynamic(() => import('../components/footer'))
-const Switcher = dynamic(() => import('../components/switcher'))
+const NavLight = dynamic(()=>import('../../components/navlight'))
+const Footer = dynamic(() => import('../../components/footer'))
+const Switcher = dynamic(() => import('../../components/switcher'))
 
-import {FiHelpCircle, FiBookmark, FiSettings, FiHexagon,MdKeyboardArrowDown} from '../assets/icons/vander'
+import {FiHelpCircle, FiBookmark, FiSettings, FiHexagon,MdKeyboardArrowDown} from '../../assets/icons/vander'
 
 export default function Helpcenter(){
     useEffect(() => {
