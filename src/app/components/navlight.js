5c49e05b41fc6b5a79dba152bc4d63bfddc16546
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-09-05 20:05:44 +0500

    Update navlight.js

--- src/app/components/navlight.js
+++ src/app/components/navlight.js
@@ -6,110 +6,114 @@ export default function NavLight(){
     const [toggleMenu, setToggleMenu] = useState(false)
     const [scroll, setScroll] = useState(false);
 
-    useEffect(()=>{
-        activateMenu()
-        window.addEventListener("scroll", () => {
-            setScroll(window.scrollY > 50);
-          });
-    },[])
-
-     /*********************/
- /*    Menu Active    */
- /*********************/
- function getClosest(elem, selector) {
- 
-    // Element.matches() polyfill
-    if (!Element.prototype.matches) {
-        Element.prototype.matches =
-            Element.prototype.matchesSelector ||
-            Element.prototype.mozMatchesSelector ||
-            Element.prototype.msMatchesSelector ||
-            Element.prototype.oMatchesSelector ||
-            Element.prototype.webkitMatchesSelector ||
-            function (s) {
-                var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-                    i = matches.length;
-                while (--i >= 0 && matches.item(i) !== this) {}
-                return i > -1;
-            };
-    }
-
-    // Get the closest matching element
-    for (; elem && elem !== document; elem = elem.parentNode) {
-        if (elem.matches(selector)) return elem;
-    }
-    return null;
-
-};
-
-function activateMenu() {
-    var menuItems = document.getElementsByClassName("sub-menu-item");
-    if (menuItems) {
-
-        var matchingMenuItem = null;
-        for (var idx = 0; idx < menuItems.length; idx++) {
-            if (menuItems[idx].href === window.location.href) {
-                matchingMenuItem = menuItems[idx];
+    useEffect(() => {
+        /*********************/
+        /*    Menu Active    */
+        /*********************/
+        function getClosest(elem, selector) {
+            // Element.matches() polyfill
+            if (!Element.prototype.matches) {
+                Element.prototype.matches =
+                    Element.prototype.matchesSelector ||
+                    Element.prototype.mozMatchesSelector ||
+                    Element.prototype.msMatchesSelector ||
+                    Element.prototype.oMatchesSelector ||
+                    Element.prototype.webkitMatchesSelector ||
+                    function (s) {
+                        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                            i = matches.length;
+                        while (--i >= 0 && matches.item(i) !== this) {}
+                        return i > -1;
+                    };
             }
+
+            // Get the closest matching element
+            for (; elem && elem !== document; elem = elem.parentNode) {
+                if (elem.matches(selector)) return elem;
+            }
+            return null;
         }
 
-        if (matchingMenuItem) {
-            matchingMenuItem.classList.add('active');
-         
-         
-            var immediateParent = getClosest(matchingMenuItem, 'li');
-      
-            if (immediateParent) {
-                immediateParent.classList.add('active');
-            }
-            
-            var parent = getClosest(immediateParent, '.child-menu-item');
-            if(parent){
-                parent.classList.add('active');
-            }
-
-            var parent = getClosest(parent || immediateParent , '.parent-menu-item');
-        
-            if (parent) {
-                parent.classList.add('active');
-
-                var parentMenuitem = parent.querySelector('.menu-item');
-                if (parentMenuitem) {
-                    parentMenuitem.classList.add('active');
-                }
-
-                var parentOfParent = getClosest(parent, '.parent-parent-menu-item');
-                if (parentOfParent) {
-                    parentOfParent.classList.add('active');
-                }
-            } else {
-                var parentOfParent = getClosest(matchingMenuItem, '.parent-parent-menu-item');
-                if (parentOfParent) {
-                    parentOfParent.classList.add('active');
-                }
-            }
-        }
-    }
-}
-/*********************/
-/*  Clickable manu   */
-/*********************/
-if (typeof window !== "undefined"){
-    if (document.getElementById("navigation")) {
-        const anchorArray = Array.from(document.getElementById("navigation").getElementsByTagName("a"));
-        anchorArray.forEach(element => {
-            element.addEventListener('click', (elem) => {
-                const target = elem.target.getAttribute("href")
-                if (target !== "") {
-                    if (elem.target.nextElementSibling) {
-                        var submenu = elem.target.nextElementSibling.nextElementSibling;
-                        submenu.classList.toggle('open');
+        function activateMenu() {
+            var menuItems = document.getElementsByClassName("sub-menu-item");
+            if (menuItems) {
+                var matchingMenuItem = null;
+                for (var idx = 0; idx < menuItems.length; idx++) {
+                    if (menuItems[idx].href === window.location.href) {
+                        matchingMenuItem = menuItems[idx];
                     }
                 }
-            })
-        });
-    }
-}
+
+                if (matchingMenuItem) {
+                    matchingMenuItem.classList.add('active');
+
+                    var immediateParent = getClosest(matchingMenuItem, 'li');
+
+                    if (immediateParent) {
+                        immediateParent.classList.add('active');
+                    }
+                    
+                    var parent = getClosest(immediateParent, '.child-menu-item');
+                    if(parent){
+                        parent.classList.add('active');
+                    }
+
+                    var parent = getClosest(parent || immediateParent , '.parent-menu-item');
+                
+                    if (parent) {
+                        parent.classList.add('active');
+
+                        var parentMenuitem = parent.querySelector('.menu-item');
+                        if (parentMenuitem) {
+                            parentMenuitem.classList.add('active');
+                        }
+
+                        var parentOfParent = getClosest(parent, '.parent-parent-menu-item');
+                        if (parentOfParent) {
+                            parentOfParent.classList.add('active');
+                        }
+                    } else {
+                        var parentOfParent = getClosest(matchingMenuItem, '.parent-parent-menu-item');
+                        if (parentOfParent) {
+                            parentOfParent.classList.add('active');
+                        }
+                    }
+                }
+            }
+        }
+
+        activateMenu();
+        
+        const handleScroll = () => {
+            setScroll(window.scrollY > 50);
+        };
+        
+        window.addEventListener("scroll", handleScroll);
+        
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    /*********************/
+    /*  Clickable menu   */
+    /*********************/
+    useEffect(() => {
+        if (typeof window !== "undefined"){
+            if (document.getElementById("navigation")) {
+                const anchorArray = Array.from(document.getElementById("navigation").getElementsByTagName("a"));
+                anchorArray.forEach(element => {
+                    element.addEventListener('click', (elem) => {
+                        const target = elem.target.getAttribute("href")
+                        if (target !== "") {
+                            if (elem.target.nextElementSibling) {
+                                var submenu = elem.target.nextElementSibling.nextElementSibling;
+                                submenu.classList.toggle('open');
+                            }
+                        }
+                    })
+                });
+            }
+        }
+    }, []);
 
     return(
         <>
