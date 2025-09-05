c33c4aca28fe968513a15725f970e6890d7fb5c2
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-09-05 20:08:11 +0500

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -14,107 +14,113 @@ export default function Navbar() {
   const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
-    activateMenu();
-    window.addEventListener('scroll', () => {
-      setScroll(window.scrollY > 50);
-    });
-    return () => window.removeEventListener('scroll', () => {});
-  }, []);
-
-  /*********************/
-  /*    Menu Active    */
-  /*********************/
-  function getClosest(elem, selector) {
-    // Element.matches() polyfill
-    if (!Element.prototype.matches) {
-      Element.prototype.matches =
-        Element.prototype.matchesSelector ||
-        Element.prototype.mozMatchesSelector ||
-        Element.prototype.msMatchesSelector ||
-        Element.prototype.oMatchesSelector ||
-        Element.prototype.webkitMatchesSelector ||
-        function (s) {
-          var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-            i = matches.length;
-          while (--i >= 0 && matches.item(i) !== this) {}
-          return i > -1;
-        };
-    }
-
-    // Get the closest matching element
-    for (; elem && elem !== document; elem = elem.parentNode) {
-      if (elem.matches(selector)) return elem;
-    }
-    return null;
-  }
-
-  function activateMenu() {
-    var menuItems = document.getElementsByClassName('sub-menu-item');
-    if (menuItems) {
-      var matchingMenuItem = null;
-      for (var idx = 0; idx < menuItems.length; idx++) {
-        if (menuItems[idx].href === window.location.href) {
-          matchingMenuItem = menuItems[idx];
-        }
+    /*********************/
+    /*    Menu Active    */
+    /*********************/
+    function getClosest(elem, selector) {
+      // Element.matches() polyfill
+      if (!Element.prototype.matches) {
+        Element.prototype.matches =
+          Element.prototype.matchesSelector ||
+          Element.prototype.mozMatchesSelector ||
+          Element.prototype.msMatchesSelector ||
+          Element.prototype.oMatchesSelector ||
+          Element.prototype.webkitMatchesSelector ||
+          function (s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+              i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;
+          };
       }
 
-      if (matchingMenuItem) {
-        matchingMenuItem.classList.add('active');
+      // Get the closest matching element
+      for (; elem && elem !== document; elem = elem.parentNode) {
+        if (elem.matches(selector)) return elem;
+      }
+      return null;
+    }
 
-        var immediateParent = getClosest(matchingMenuItem, 'li');
-
-        if (immediateParent) {
-          immediateParent.classList.add('active');
+    function activateMenu() {
+      var menuItems = document.getElementsByClassName('sub-menu-item');
+      if (menuItems) {
+        var matchingMenuItem = null;
+        for (var idx = 0; idx < menuItems.length; idx++) {
+          if (menuItems[idx].href === window.location.href) {
+            matchingMenuItem = menuItems[idx];
+          }
         }
 
-        var parent = getClosest(immediateParent, '.child-menu-item');
-        if (parent) {
-          parent.classList.add('active');
-        }
+        if (matchingMenuItem) {
+          matchingMenuItem.classList.add('active');
 
-        var parentOfParent = getClosest(parent || immediateParent, '.parent-menu-item');
+          var immediateParent = getClosest(matchingMenuItem, 'li');
 
-        if (parentOfParent) {
-          parentOfParent.classList.add('active');
-
-          var parentMenuitem = parentOfParent.querySelector('.menu-item');
-          if (parentMenuitem) {
-            parentMenuitem.classList.add('active');
+          if (immediateParent) {
+            immediateParent.classList.add('active');
           }
 
-          var grandParent = getClosest(parentOfParent, '.parent-parent-menu-item');
-          if (grandParent) {
-            grandParent.classList.add('active');
+          var parent = getClosest(immediateParent, '.child-menu-item');
+          if (parent) {
+            parent.classList.add('active');
           }
-        } else {
-          var parentOfParent = getClosest(matchingMenuItem, '.parent-parent-menu-item');
+
+          var parentOfParent = getClosest(parent || immediateParent, '.parent-menu-item');
+
           if (parentOfParent) {
             parentOfParent.classList.add('active');
+
+            var parentMenuitem = parentOfParent.querySelector('.menu-item');
+            if (parentMenuitem) {
+              parentMenuitem.classList.add('active');
+            }
+
+            var grandParent = getClosest(parentOfParent, '.parent-parent-menu-item');
+            if (grandParent) {
+              grandParent.classList.add('active');
+            }
+          } else {
+            var parentOfParent = getClosest(matchingMenuItem, '.parent-parent-menu-item');
+            if (parentOfParent) {
+              parentOfParent.classList.add('active');
+            }
           }
         }
       }
     }
-  }
+
+    activateMenu();
+    
+    const handleScroll = () => {
+      setScroll(window.scrollY > 50);
+    };
+    
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   /*********************/
   /*  Clickable menu   */
   /*********************/
-  if (typeof window !== 'undefined') {
-    if (document.getElementById('navigation')) {
-      const anchorArray = Array.from(document.getElementById('navigation').getElementsByTagName('a'));
-      anchorArray.forEach((element) => {
-        element.addEventListener('click', (elem) => {
-          const target = elem.target.getAttribute('href');
-          if (target !== '') {
-            if (elem.target.nextElementSibling) {
-              var submenu = elem.target.nextElementSibling.nextElementSibling;
-              submenu.classList.toggle('open');
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (document.getElementById('navigation')) {
+        const anchorArray = Array.from(document.getElementById('navigation').getElementsByTagName('a'));
+        anchorArray.forEach((element) => {
+          element.addEventListener('click', (elem) => {
+            const target = elem.target.getAttribute('href');
+            if (target !== '') {
+              if (elem.target.nextElementSibling) {
+                var submenu = elem.target.nextElementSibling.nextElementSibling;
+                submenu.classList.toggle('open');
+              }
             }
-          }
+          });
         });
-      });
+      }
     }
-  }
+  }, []);
 
   return (
     <>
